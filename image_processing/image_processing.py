e1f9573a827f9b7d1749fb1348304a63ae993dbd
Marco Formoso <marco.a.formoso@gmail.com>
2016-09-07 14:36:27 +0200

    Face detection and masking

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -1,6 +1,6 @@
 import cv2
 import imutils
-
+import numpy as np
 
 
 def simple_return(image):
@@ -13,17 +13,20 @@ def crop_image(image):
 
 detector = cv2.CascadeClassifier('image_processing/cascades/haarcascade_frontalface_default.xml')
 
-def face_detection(image):
 
+def face_detection(image):
     image = imutils.resize(image, width=300)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faceRects = detector.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5,
                                           minSize=(30, 30), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
 
     # loop over the faces and draw a rectangle around each
+    mask = np.zeros(image.shape[:2], dtype='uint8')
     for (x, y, w, h) in faceRects:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(mask, (x, y), (x + w, y + h), 255, -1)
 
+    image = cv2.bitwise_and(image, image, mask=mask)
 
     return image
+
 
