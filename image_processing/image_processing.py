2bac28aa93f4201e7c5aeda5180d96a416031f92
Marco Formoso <marco.a.formoso@gmail.com>
2019-03-18 09:09:26 +0100

    Updated to use OpenCv 4.0

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -18,7 +18,7 @@ def face_detection(image):
     image = imutils.resize(image, width=300)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faceRects = detector.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5,
-                                          minSize=(30, 30), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+                                          minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 
     # loop over the faces and draw a rectangle around each
     mask = np.zeros(image.shape[:2], dtype='uint8')
